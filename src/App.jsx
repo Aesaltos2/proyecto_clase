92f9a590e63268b012a1c61bdec9e80f5a0bcf1c
Adrian Saltos <Asaltos>
2024-08-21 15:41:26 -0500

    Primera Prueba de funcionalidad

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import LoginPage from './Pages/LoginPage'
+import Rediriguir from './Pages/Rediriguir'
+import Proteger from './Pages/Proteger'
+import DashboardPageStudent from './Pages/Student/DashboardPageStudent'
 
 export default function App() {
   return (
-    <div>App</div>
+    <Routes>
+      <Route path='/' element={ <Rediriguir/>}/>
+      <Route path='/login' element={<LoginPage />} />
+      {/* <Route  element={<Proteger/>} > */}
+        <Route path='/dashboard' element={<DashboardPageStudent/>} />
+      {/* </Route> */}
+    </Routes>
   )
 }
 
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
+import { Authcontext } from '../context/AuthContext'
 
 const Login = () => {
-    const [showPassword, setShowPassword] = useState(false);
+  const { Login } = useContext(Authcontext)
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleLogin = async function (e) {
+    e.preventDefault()
+
+    const data = {
+      email: e.target.email.value,
+      password: e.target.password.value
+    }
+
+    await Login.mutateAsync(data)
+  }
 
   return (
     <main className="bg-[url('/wallpaper-login.jpg')] h-screen w-screen flex justify-center items-center">
@@ -11,7 +24,7 @@ const Login = () => {
             <img src="/logo 2023.png" alt="" className="w-full h-full" />
           </figure>
 
-          <form
+          <form onSubmit={handleLogin}
             action=""
             className="flex flex-col justify-between gap-2 p-[35px] h-[80%] w-full "
           >
@@ -36,49 +49,50 @@ const Login = () => {
                 placeholder="password"
                 className="outline-none"
               />
-                      <div
-          onClick={() => setShowPassword(!showPassword)}
-          className="cursor-pointer"
-        >
-          {showPassword ? (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="currentColor"
-              class="size-6"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88"
-              />
-            </svg>
-          ) : (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="currentColor"
-              class="size-6"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z"
-              />
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
-              />
-            </svg>
-          )}
-        </div>
+              <div
+                onClick={() => setShowPassword(!showPassword)}
+                className="cursor-pointer"
+              >
+                {showPassword ? (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    class="size-6"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88"
+                    />
+                  </svg>
+                ) : (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    class="size-6"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z"
+                    />
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                    />
+                  </svg>
+                )}
+              </div>
             </label>
             <button
+            onSubmit={handleLogin}
               type="submit"
               className=" p-[2%] rounded-lg bg-blue-700 text-white font-medium"
             >
