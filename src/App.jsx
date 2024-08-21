1686837f5c07f0247cc76bda4ffea53fe707fec7
Adrian Saltos <Asaltos>
2024-08-21 16:34:25 -0500

    Upload Funcionalidades profesor

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,7 @@
 import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import LoginPage from './Pages/LoginPage'
-import Rediriguir from './Pages/Rediriguir'
-import Proteger from './Pages/Proteger'
-import DashboardPageStudent from './Pages/Student/DashboardPageStudent'
-import Examenespages from './Pages/Student/Examenespages'
-import Exampage from './Pages/Student/Exampage'
-import VideoPage from './Pages/Student/VideoPage'
-import DashboardPageTeacher from './Pages/Teacher/DashboardPageTeacher'
 
 export default function App() {
   return (
-    <Routes>
-      <Route path='/' element={ <Rediriguir/>}/>
-      <Route path='/login' element={<LoginPage />} />
-      {/* <Route  element={<Proteger/>} > */}
-        <Route path='/dashboard' element={<DashboardPageStudent/>} />
-        <Route path='/examenes' element={<Examenespages/>} />
-        <Route path='/examen/:id' element={<Exampage/>} />
-        <Route path='/videos/me' element={<VideoPage/>} />
-        <Route path='/teacher/dashboard' element={<DashboardPageTeacher/>}/>
-      {/* </Route> */}
-    </Routes>
+    <div>App</div>
   )
 }
--- src/Pages/Teacher/ExamenesTeacherPages.jsx
+++ src/Pages/Teacher/ExamenesTeacherPages.jsx
@@ -1,17 +1,16 @@
 import React from 'react'
-import AlumnosTabla from '../../components/Teacher/AlumnosTabla'
 import Header from '../../components/Header';
 import Navbar from '../../components/NavbarTeacher';
+import StudentCard from '../../components/Student/studentCard';
 
-export default function AlumnosTable() {
+export default function ExamenesTeacherPages() {
   return (
     <div className="h-screen flex overflow-hidden">
-      <Navbar className="w-1/4" />
-      <div className="flex flex-1 flex-col">
+    <Navbar className="w-1/4" />
+    <div className="flex flex-1 flex-col">
         <Header className="w-full flex-shrink-0" />
-        <AlumnosTabla />
-      </div>
+        <StudentCard/>
     </div>
-
+</div>
   )
 }
