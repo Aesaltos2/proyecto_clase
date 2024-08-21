4f202332cd8eb94e2ffbd983794ba2be7b1f22b8
Adrian Saltos <Asaltos>
2024-08-21 10:25:39 -0500

    Upload App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
 import React from 'react'
-import Login from './components/Login'
 
 export default function App() {
   return (
-    <div>
-      <Login />
-    </div>
+    <div>App</div>
   )
 }
-
